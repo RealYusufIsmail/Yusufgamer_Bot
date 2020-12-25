1eaa2b1f45160273cbfbc4b75600646734eaa571
realyusufismail <67903886+realyusufismail@users.noreply.github.com>
2020-12-25 20:00:15 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -239,7 +239,21 @@ else if(isValidCommand(message,"unmute")){
     }
   }
   
-  
+  else if(isValidCommand(message,"Invitelink")){
+     f(message.member.hasPermission(['CREATE_INSTANT_INVITE'])){
+    message.channel.send("You dont have permission to unmute people!");
+  }
+     let invite = await message.channel.createInvite(
+  {
+    maxAge: 10 * 60 * 1000, // maximum time for the invite, in milliseconds
+    maxUses: 1 // maximum times it can be used
+  },
+  `Requested with command by ${message.author.tag}`
+)
+.catch(console.log);
+
+  message.reply(invite ? `Here's your invite: ${invite}` : "There has been an error during the creation of the invite.");
+}
 }
 });
  
