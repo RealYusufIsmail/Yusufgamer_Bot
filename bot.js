47d00df09051d4f7df3b5bf8e3a0e1e0b85c2116
realyusufismail <67903886+realyusufismail@users.noreply.github.com>
2021-02-03 17:11:36 +0000

    b

--- bot.js
+++ bot.js
@@ -88,6 +88,13 @@ else if(isValidCommand(message, "number-of-invites")){
       }
   )
 }
+
+else if(isValidCommend(message,"invitelink")){
+  channel.createInvite()
+  .then(invite => console.log(`Created an invite with a code of ${invite.code}`))
+  .catch(console.error);
+
+}
 else if(isValidCommand(message, "del")){
    let args = message.content.toLowerCase().substring(5);
    let roleNames = args.split(", ");
